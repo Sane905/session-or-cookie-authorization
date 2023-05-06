daacbbe45e422c248596c02c9bb12a9a4c74dcfc
Sane905 <konjaku0905@yahoo.co.jp>
2023-05-06 21:06:59 +0900

    fix:gitからファイルを除く

--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -2,6 +2,9 @@ export const pagesPath = {
   "home": {
     $url: (url?: { hash?: string }) => ({ pathname: '/home' as const, hash: url?.hash })
   },
+  "sign_in": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/sign-in' as const, hash: url?.hash })
+  },
   $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
 }
 
